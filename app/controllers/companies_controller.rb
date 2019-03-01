a92cca23e978056b8ed54a7879ef220666e19bb6
Dana Simmons <dana@simmons-audio.com>
2019-02-28 20:43:41 -0500

    got create and show page working added model method for getting floors

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -7,17 +7,29 @@ class CompaniesController < ApplicationController
         # @company.buildings.build
         render "new"
     end
-
+    def show
+        @company = Company.find(params[:id])
+        render "show"
+    end
     def create
         # We want { 'building_id' => [floor1, floor2, ...]}
         # Iterate over keys as building_id
         #   then iterate over value as floors
         #   and create office for each
-        # company_params[:offices_attributes].each do |key,vaule|
+        @company = Company.create(name: company_params[:name]) 
+        company_params[:offices_attributes].each do |_, value| 
+            building = Building.find(value["id"])
+            if value.has_key?("offices")
+                value["offices"].each do |floor|
+                    office = Office.new(floor: floor)
+                    office.building = building
+                    office.company = @company
+                    office.save
+                end
+            end
 
-        #         office[:building_id] = valu[:id]
-        #         office[:floor] = 
-        binding.pry
+        end
+        redirect_to company_path(@company)
     end
 
     private
--- app/models/company.rb
+++ app/models/company.rb
@@ -3,4 +3,12 @@ class Company < ApplicationRecord
     has_many :buildings, through: :offices
     has_many :employees
     accepts_nested_attributes_for :offices
+
+    def get_floors
+        self.offices.inject({}) do |res, office|
+            res[office.building.name] ||= []
+            res[office.building.name] << office.floor
+            res
+        end
+    end
 end
